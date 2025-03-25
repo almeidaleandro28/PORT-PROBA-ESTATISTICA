a6f9b6ee2656369e67dfb87fea55b436d2ef0ae3
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2025-03-24 23:28:00 -0300

    feat: RESPONSE THIRTH QUESTION

--- portfolio.R
+++ portfolio.R
@@ -17,4 +17,30 @@ nameStates <- max( table(df$uf) )
 # -------------------------------------------------------------------------------------------------
 weatherCodition <- df$condicao_metereologica
 contWeatherCodition <- table( weatherCodition )
-print( contWeatherCodition )
+
+contWeatherLight <- sum( weatherCodition == "C\xe9u Claro" )
+contAcidents <- nrow( df )
+
+# probability of an accident happening in clear weather
+probAcidentWetherLight <- ( contWeatherLight / contAcidents ) * 100
+
+# print( probAcidentWetherLight )
+# cat("Probabilidade de ocorre um acidente no clima claro ", probAcidentWetherLight, "% ")
+
+# ------------------------------------------------
+contWeatherSun <- ( sum( weatherCodition == "Sol") / contAcidents )
+contWeatherRain <-( sum( weatherCodition == "Chuva") / contAcidents )
+contWeatherWind <- ( sum( weatherCodition == "Vento") / contAcidents )
+contWeatherHail <- (sum( weatherCodition == "Granizo ") / contAcidents )
+contWeatherSnow <- (sum( weatherCodition == "Neve") / contAcidents )
+contWeatherDrizzle <- (sum( weatherCodition == "Garoa/Chuvisco") / contAcidents )
+contWeatherCloudy <- (sum( weatherCodition == " Nublado") / contAcidents )
+contWeaherFogMist <- (sum( weatherCodition == "Nevoeiro/Neblina") / contAcidents ) 
+contWeatherIgnored <- (sum( weatherCodition == " Ignorado")  / contAcidents)
+
+# cat("Tempo com sol", contWeatherSun)
+#-----------------------------------------------------------
+# como a fase do dia afeta a ocorrência de acidentes
+dayPhase <- df$fase_dia
+contDayPhase <- table( dayPhase )
+print( contDayPhase  )
